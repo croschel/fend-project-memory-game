4a6d82a1d24ad3342e8887b017ce15d31ec91401
croschel <36478774+croschel@users.noreply.github.com>
2019-01-02 11:20:25 -0200

    My cards start to turn on click

--- js/app.js
+++ js/app.js
@@ -25,6 +25,12 @@ function shuffle(array) {
     return array;
 }
 
+function turnCard(){
+        $('li.card').click(function(){
+            $(this).addClass("open show");
+        });
+}
+
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -36,3 +42,5 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+turnCard();
